836f5ee37e626f4482196da80bfe5e5dbafce60e
Sergey Sinyakov <1103cc@mail.ru>
2022-03-01 10:34:05 +0300

    BIAT-18
    BIAT-18: создать проект на spring

---
+++ src/main/java/me/biathlonvsu/biathlon/BiathlonApplication.java
@@ -0,0 +1,13 @@
+package me.biathlonvsu.biathlon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiathlonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiathlonApplication.class, args);
+	}
+
+}

da20c834fe0ce19332693691fc71c7bc030c2925
Sergey <1103cc@mail.ru>
2022-04-04 16:00:12 +0300

    BIAT-31:реализовал Controller для Competition

---
+++ src/main/java/me/biathlonvsu/biathlon/Controller/CompetitionController.java
@@ -0,0 +1,30 @@
+package me.biathlonvsu.biathlon.Controller;
+
+
+import lombok.RequiredArgsConstructor;
+import me.biathlonvsu.biathlon.Service.CompetitionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CompetitionController {
+
+    private CompetitionService competitionService;
+
+    @CrossOrigin(origins = "http://localhost:63342")//для фронта в идеи
+    @GetMapping("/competition/{id}")
+    public ResponseEntity<?> fetchCompetition(@PathVariable int id){
+        return ResponseEntity.ok(competitionService.getCompetitionById(id));
+    }
+
+    @CrossOrigin(origins = "http://localhost:63342")//для фронта в идеи
+    @GetMapping("/competitions")
+    public ResponseEntity<?> fetchCompetitions(){
+        return ResponseEntity.ok(competitionService.getAllCompetitions());
+    }
+
+}
